9be9f36d41ac4f67cc0210e3d5b3c7e58793fe92
Efe <efeincucuk20@gmail.com>
2024-07-11 01:43:51 +0300

    Çalışma tamamlandı

---
+++ MatrisÇalışma/src/App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int matris[][] = new int[3][4];
+        Scanner input = new Scanner(System.in);
+        int number = 0;
+
+        for(int i = 0; i < matris.length; i++){
+            for(int j = 0; j < matris[0].length; j++){
+                System.out.print("Matirisin " + (i+1) + ". satırı " + (j+1) + ". sütünun değerini giriniz: ");
+                number = input.nextInt();
+                matris[i][j] = number;
+            }
+        }
+
+        for(int i = 0; i < matris.length; i++){
+            System.out.println();
+            for(int j = 0; j < matris[0].length; j++){
+                System.out.print(matris[i][j] + " ");
+            }
+        }
+    }
+}
