5c620ad3a8c0aa13bc61ef23fe6802454669c896
Efe <efeincucuk20@gmail.com>
2024-07-13 04:01:03 +0300

    Proje Tamamlandı

---
+++ ArraySort/src/App.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class App {
+    
+    static int[] sortedArray(int list[]){
+        int tempValue;
+        
+        for(int i = 0; i < list.length; i++){
+            for(int j = i; j < list.length; j++){
+                if(list[i] > list[j]){
+                    tempValue = list[i];
+                    list[i] = list[j];
+                    list[j] = tempValue;
+                }
+            }
+        }
+
+        return list;
+    }
+    public static void main(String[] args) throws Exception {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Dizinin Boyutu n : ");
+        int n = input.nextInt();
+
+        int[] list = new int[n];
+
+        System.out.println("Dizinin elemanlarını giriniz :");
+
+        for(int i = 0; i < list.length; i++){
+            System.out.print((i+1) + ". elemanı : ");
+            list[i] = input.nextInt();
+        }
+
+        sortedArray(list);
+
+        System.out.println("Sıralama : " + Arrays.toString(list));
+
+    }
+}
